d796bb43a89cdd53ee74110138ab404ac3557929
wiley68 <ilko.iv@gmail.com>
2020-09-16 08:46:23 +0300

    add vue @ auth

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'IndexController@index')->name('index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
